105589bbdab1c384d48322daca14b7e76a5d591a
kowersky255 <franckpatrick58@gmail.com>
2024-11-29 09:35:34 +0300

    updated merge functionality

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const splitRoute = require("./routes/split");
 const mergeRoute = require("./routes/merge");
+const compressRoute = require('./routes/compress')
 
 const app = express();
 
 //Middleware
 app.use(cors());
-app.use(express.json());
 
 app.use(mergeRoute);
 app.use(splitRoute);
+app.use(compressRoute);
 
 app.listen(5000, () => {
   console.log("Server has started running on port 5000");
 });
+
